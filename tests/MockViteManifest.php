3028e6af8c08feaa47074d043e20f8aab522f75c
Sympky <popamarianmario@gmail.com>
2025-03-12 22:13:43 +0200

    fix github workflow

--- tests/MockViteManifest.php
+++ tests/MockViteManifest.php
@@ -82,6 +82,88 @@ class MockViteManifest
                             'src' => 'resources/js/pages/dashboard.tsx',
                             'isEntry' => true,
                         ],
+                        // Report pages
+                        'resources/js/pages/reports/templates/index.tsx' => [
+                            'file' => 'assets/reports-templates-index-mock.js',
+                            'src' => 'resources/js/pages/reports/templates/index.tsx',
+                            'isEntry' => true,
+                        ],
+                        'resources/js/pages/reports/templates/create.tsx' => [
+                            'file' => 'assets/reports-templates-create-mock.js',
+                            'src' => 'resources/js/pages/reports/templates/create.tsx',
+                            'isEntry' => true,
+                        ],
+                        'resources/js/pages/reports/templates/edit.tsx' => [
+                            'file' => 'assets/reports-templates-edit-mock.js',
+                            'src' => 'resources/js/pages/reports/templates/edit.tsx',
+                            'isEntry' => true,
+                        ],
+                        'resources/js/pages/reports/templates/show.tsx' => [
+                            'file' => 'assets/reports-templates-show-mock.js',
+                            'src' => 'resources/js/pages/reports/templates/show.tsx',
+                            'isEntry' => true,
+                        ],
+                        'resources/js/pages/reports/create/SelectTemplate.tsx' => [
+                            'file' => 'assets/reports-create-selecttemplate-mock.js',
+                            'src' => 'resources/js/pages/reports/create/SelectTemplate.tsx',
+                            'isEntry' => true,
+                        ],
+                        'resources/js/pages/reports/create/SelectClientProject.tsx' => [
+                            'file' => 'assets/reports-create-selectclientproject-mock.js',
+                            'src' => 'resources/js/pages/reports/create/SelectClientProject.tsx',
+                            'isEntry' => true,
+                        ],
+                        'resources/js/pages/reports/create/AddDetails.tsx' => [
+                            'file' => 'assets/reports-create-adddetails-mock.js',
+                            'src' => 'resources/js/pages/reports/create/AddDetails.tsx',
+                            'isEntry' => true,
+                        ],
+                        'resources/js/pages/reports/index.tsx' => [
+                            'file' => 'assets/reports-index-mock.js',
+                            'src' => 'resources/js/pages/reports/index.tsx',
+                            'isEntry' => true,
+                        ],
+                        'resources/js/pages/reports/edit.tsx' => [
+                            'file' => 'assets/reports-edit-mock.js',
+                            'src' => 'resources/js/pages/reports/edit.tsx',
+                            'isEntry' => true,
+                        ],
+                        'resources/js/pages/reports/show.tsx' => [
+                            'file' => 'assets/reports-show-mock.js',
+                            'src' => 'resources/js/pages/reports/show.tsx',
+                            'isEntry' => true,
+                        ],
+                        // Vulnerability pages
+                        'resources/js/pages/VulnerabilityTemplates/Index.tsx' => [
+                            'file' => 'assets/vulnerability-templates-index-mock.js',
+                            'src' => 'resources/js/pages/VulnerabilityTemplates/Index.tsx',
+                            'isEntry' => true,
+                        ],
+                        'resources/js/pages/VulnerabilityTemplates/Edit.tsx' => [
+                            'file' => 'assets/vulnerability-templates-edit-mock.js',
+                            'src' => 'resources/js/pages/VulnerabilityTemplates/Edit.tsx',
+                            'isEntry' => true,
+                        ],
+                        'resources/js/pages/Vulnerabilities/Index.tsx' => [
+                            'file' => 'assets/vulnerabilities-index-mock.js',
+                            'src' => 'resources/js/pages/Vulnerabilities/Index.tsx',
+                            'isEntry' => true,
+                        ],
+                        'resources/js/pages/Vulnerabilities/Create.tsx' => [
+                            'file' => 'assets/vulnerabilities-create-mock.js',
+                            'src' => 'resources/js/pages/Vulnerabilities/Create.tsx',
+                            'isEntry' => true,
+                        ],
+                        'resources/js/pages/Vulnerabilities/Edit.tsx' => [
+                            'file' => 'assets/vulnerabilities-edit-mock.js',
+                            'src' => 'resources/js/pages/Vulnerabilities/Edit.tsx',
+                            'isEntry' => true,
+                        ],
+                        'resources/js/pages/Vulnerabilities/Show.tsx' => [
+                            'file' => 'assets/vulnerabilities-show-mock.js',
+                            'src' => 'resources/js/pages/Vulnerabilities/Show.tsx',
+                            'isEntry' => true,
+                        ],
                     ];
                 }
                 
